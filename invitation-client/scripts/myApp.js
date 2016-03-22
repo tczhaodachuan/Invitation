50f5c898e3f401ac07f0f5573ed26636acf104e8
tczhaodachuan <tczhaodachuan@gmail.com>
2016-03-21 23:44:14 -0400

    changes

--- invitation-client/scripts/myApp.js
+++ invitation-client/scripts/myApp.js
@@ -25,9 +25,33 @@ myApp.controller('formController', ['$scope', '$http', function ($scope, $http) 
             // called asynchronously if an error occurs
             // or server returns response with an error status.
             console.log(response)
-            if (response.status == 500) {
-                $scope.failed = true
+            $scope.failed = true
+        });
+    }
+
+
+    $scope.cancel = function (user) {
+        console.log(user)
+        $http({
+            method: 'DELETE',
+            url: '/rsvp/rest/guest',
+            data: user,
+            headers: {
+                'Content-Type': 'application/json'
             }
+        }).then(function successCallback(response) {
+            // this callback will be called asynchronously
+            // when the response is available
+            console.log(response)
+            if (response.status == 204) {
+                $scope.cancelled = true
+                $scope.user = null
+            }
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            console.log(response)
+            $scope.failed = true
         });
     }
 
