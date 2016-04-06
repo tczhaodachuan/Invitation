b13860b0ac26a09f96848558332ad5d208d25a18
tczhaodachuan <tczhaodachuan@gmail.com>
2016-04-05 22:41:42 -0400

    loading

--- invitation-client/scripts/loading.js
+++ invitation-client/scripts/loading.js
@@ -1,17 +1,9 @@
 $(window).on('load', function () {
     // cover page
     $("#cover").fadeOut(200);
-    // navigation control
     $(".btn-nav").on("click tap", function () {
         $(".nav-container").toggleClass("showNav hideNav").removeClass("hidden");
         $(this).toggleClass("animated");
-    });
-
-    jQuery(document).ready(function ($) {
-        $(".scroll, .navbar li a, .footer li a").click(function (event) {
-            event.preventDefault();
-            $('html,body').animate({scrollTop: $(this.hash).offset().top}, 1000);
-        });
     });
     // count down starts
     $("#countdown").countdown({
