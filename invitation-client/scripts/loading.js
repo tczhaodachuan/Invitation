b96bd3e0eb75c6f0301ec3488cbd16b0dcdec71e
tczhaodachuan <tczhaodachuan@gmail.com>
2016-04-05 22:56:30 -0400

    loading

--- invitation-client/scripts/loading.js
+++ invitation-client/scripts/loading.js
@@ -1,10 +1,6 @@
 $(window).on('load', function () {
     // cover page
     $("#cover").fadeOut(200);
-    $(".btn-nav").on("click tap", function () {
-        $(".nav-container").toggleClass("showNav hideNav").removeClass("hidden");
-        $(this).toggleClass("animated");
-    });
     // count down starts
     $("#countdown").countdown({
             date: "21 may 2016 11:18:00",
